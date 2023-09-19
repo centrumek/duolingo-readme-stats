7ed8b1ea2b6eb1ed1a6e42bbb62b89e0696dfd36
Paul Potaczała <paul.potaczala@gmail.com>
2023-09-19 20:19:52 +0200

    Add commits

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,67 @@
-
 import {getUserDetails} from "./api";
 import * as fs from "fs";
-import {END_TOKEN, formatLanguagesTable, formatOverviewTable, INFO_LINE, START_TOKEN} from "./util";
+import {
+    commitFile,
+    END_TOKEN,
+    formatLanguagesTable,
+    formatOverviewTable,
+    INFO_LINE,
+    setFailure,
+    START_TOKEN
+} from "./util";
 import {UserDetailsResponse} from "./types";
 import {getInput} from "@actions/core";
 
 // Public parameters
-export const USER_ID = getInput('USER_ID')?.toLowerCase();
 export const FILE_NAME = getInput('FILE_NAME');
+export const COMMIT_MSG = getInput('COMMIT_MSG');
+export const COMMIT_USERNAME = getInput('COMMIT_MSG');
+export const COMMIT_EMAIL = getInput('COMMIT_MSG');
+export const IS_DEBUG = getInput('IS_DEBUG') === 'true';
+export const SHOW_LANGUAGES = getInput('SHOW_LANGUAGES') === 'true';
+export const USER_ID = getInput('USER_ID')?.toLowerCase();
 
 (async () => {
+    try {
+        if (!USER_ID) {
+            throw new Error('User ID not provided!');
+        }
+        const content = await buildContent();
+        updateFile(content);
+        if (!IS_DEBUG) {
+            await commitFile(FILE_NAME, COMMIT_MSG, COMMIT_USERNAME, COMMIT_EMAIL);
+        }
+        console.log('Successfully updated the README file!');
+    } catch (error) {
+        const errorMessage = error instanceof Error
+            ? error.message
+            : 'The action failed with an Unknown error';
+        setFailure(errorMessage);
+    }
+})()
 
+async function buildContent() {
     const content: string[] = [];
 
     const userDetails: UserDetailsResponse = await getUserDetails(USER_ID);
     content.push(formatOverviewTable(userDetails.username, userDetails.streak, userDetails.totalXp));
-    content.push(formatLanguagesTable(userDetails.courses));
 
+    if (SHOW_LANGUAGES) {
+        if (userDetails.courses.length === 0) {
+            throw new Error('No languages found!');
+        }
+        content.push(formatLanguagesTable(userDetails.courses));
+    }
+
+    return content;
+}
+
+function updateFile(content: string[]) {
     const readmeContent = fs.readFileSync('./' + FILE_NAME, 'utf-8')
 
     const startIndex = readmeContent.indexOf(START_TOKEN);
     if (startIndex === -1) {
-        throw new Error(
-            `Couldn't find the START_TOKEN ${START_TOKEN} - Exiting!`
-        );
+        throw new Error(`Couldn't find the START_TOKEN ${START_TOKEN} - Exiting!`);
     }
 
     const endIndex = readmeContent.indexOf(END_TOKEN);
@@ -32,14 +70,8 @@ export const FILE_NAME = getInput('FILE_NAME');
     }
 
     const oldPart = readmeContent.slice(startIndex, endIndex);
-
     const readmeSafeParts = readmeContent.split(oldPart);
-
-    const newReadme = `${
-        readmeSafeParts[0]
-    }${START_TOKEN}\n${INFO_LINE}\n${content.join('\n')}\n${
-        readmeSafeParts[1]
-    }`;
+    const newReadme = `${readmeSafeParts[0]}${START_TOKEN}\n${INFO_LINE}\n${content.join('\n')}\n${readmeSafeParts[1]}`;
 
     fs.writeFileSync('./' + FILE_NAME, newReadme);
-})()
+}
--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,41 @@
 import {Course} from "./types";
 import {getEmoji} from "language-flag-colors";
-
+import {spawn} from "node:child_process";
+import {setFailed} from "@actions/core";
 
 export const START_TOKEN = '<!--START_SECTION:duolingoStats-->';
 export const END_TOKEN = '<!--END_SECTION:duolingoStats-->';
 export const INFO_LINE = '<!-- Automatically generated with https://github.com/centrumek/duolingo-readme-stats-->\n';
+
+export const commitFile = async (filename: string, message: string, username: string, email: string) => {
+    await exec('git', [
+        'config',
+        '--global',
+        'user.email',
+        email
+    ]);
+    await exec('git', ['config', '--global', 'user.name', username]);
+    await exec('git', ['add', filename]);
+    await exec('git', ['commit', '-m', message]);
+    await exec('git', ['push']);
+};
+
+const exec = (cmd: string, args: string[] = []) =>
+    new Promise((resolve, reject) => {
+        const childProcess = spawn(cmd, args, {stdio: 'pipe'});
+        let stdout = '';
+        childProcess.stdout.on('data', data => {
+            stdout = data;
+        });
+        childProcess.on('close', code => {
+            if (code !== 0 && !stdout.includes('nothing to commit')) {
+                const err = new Error(`[CHILD PROCESS] Invalid status code: ${code}`);
+                return reject(err);
+            }
+            return resolve(code);
+        });
+        childProcess.on('error', reject);
+    });
 
 export const formatOverviewTable = (username: string, streak: number, totalXp: number): string => {
     const tableHeader = `| Username | Streak | Total XP |`;
@@ -28,7 +59,7 @@ export const formatLanguagesTable = (courses: Course[]): string => {
 
     const rows = courses.map(course => {
         const data = [
-            getFlagEmoji(course.title) + ' ' + course.title,
+            getEmoji(course.title) + ' ' + course.title,
             '👑 ' + course.crowns,
             '⚡ ' + course.xp
         ];
@@ -38,6 +69,7 @@ export const formatLanguagesTable = (courses: Course[]): string => {
     return `${tableHeader}\n${tableSeparator}\n${rows}\n`;
 };
 
-const getFlagEmoji = (langCode: string) : string => {
-    return getEmoji(langCode) ?? "";
-}
+export const setFailure = (error: string) => {
+    console.error(error);
+    setFailed(error);
+};
